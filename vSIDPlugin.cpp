d095698c7d31bf4d79b01994641260af7d5bc88b
Phil7789 <87429599+Phil7789@users.noreply.github.com>
2024-08-27 23:04:27 +0200

    Fixing loop when sync. states
    When a proxy instance was used syncing gnd states caused a loop freezing ES and spamming vatsim servers

--- vSIDPlugin.cpp
+++ vSIDPlugin.cpp
@@ -999,9 +999,8 @@ void vsid::VSIDPlugin::syncStates(EuroScopePlugIn::CFlightPlan FlightPlan)
 
 	if (this->processed.contains(callsign))
 	{
-		vsid::fpln::setScratchPad(FlightPlan, std::string(".vsid_state_") +
-								((FlightPlan.GetClearenceFlag()) ? "true" : "false") + "/" +
-								this->processed[callsign].gndState);
+		vsid::fpln::setScratchPad(FlightPlan, std::string(".vsid_state_") + ((FlightPlan.GetClearenceFlag()) ? "true" : "false"));		
+		if (this->processed[callsign].gndState != "") vsid::fpln::setScratchPad(FlightPlan, this->processed[callsign].gndState);
 	}
 }
 /*
@@ -1849,11 +1848,11 @@ bool vsid::VSIDPlugin::OnCompileCommand(const char* sCommandLine)
 			for (auto &elem : this->removeProcessed)
 			{
 				messageHandler->writeMessage("DEBUG", "[" + elem.first + "] being removed at: " + vsid::time::toFullString(elem.second.first) +
-											" and is disconnected " + ((elem.second.second) ? "YES" : "NO"));
+											" and is disconnected " + ((elem.second.second) ? "YES" : "NO"), vsid::MessageHandler::DebugArea::Dev);
 			}
 			if (this->removeProcessed.size() == 0)
 			{
-				messageHandler->writeMessage("DEBUG", "Removed list empty");
+				messageHandler->writeMessage("DEBUG", "Removed list empty", vsid::MessageHandler::DebugArea::Dev);
 			}
 			return true;
 		}
@@ -2324,11 +2323,11 @@ bool vsid::VSIDPlugin::OnCompileCommand(const char* sCommandLine)
 				
 				// sync states - DISABLED DUE TO UNKNOWN LOOP CAUSING ES TO STALL - REASON: Unlimited scratchpad entries and removals although not called
 
-				/*if (fpln.GetClearenceFlag() || this->processed[fp.first].gndState != "")
+				if (fpln.GetClearenceFlag() || this->processed[fp.first].gndState != "")
 				{
 					messageHandler->writeMessage("DEBUG", "[" + fp.first + "] calling sync state.", vsid::MessageHandler::DebugArea::Dev);
 					this->syncStates(fpln);
-				}*/
+				}
 			}
 			return true;
 		}
@@ -2601,7 +2600,6 @@ void vsid::VSIDPlugin::OnFlightPlanControllerAssignedDataUpdate(EuroScopePlugIn:
 		if (DataType == EuroScopePlugIn::CTR_DATA_TYPE_SCRATCH_PAD_STRING)
 		{
 			// GRP does not alway delete states so we delete if present
-
 			if (scratchpad.find("NOSTATE") != std::string::npos)
 			{
 				vsid::fpln::removeScratchPad(FlightPlan, "NOSTATE");
@@ -2622,7 +2620,6 @@ void vsid::VSIDPlugin::OnFlightPlanControllerAssignedDataUpdate(EuroScopePlugIn:
 				vsid::fpln::removeScratchPad(FlightPlan, "LINEUP");
 				this->processed[callsign].gndState = "LINEUP";
 			}
-
 
 			if (scratchpad.find(".VSID_REQ_") != std::string::npos)
 			{
@@ -2658,7 +2655,6 @@ void vsid::VSIDPlugin::OnFlightPlanControllerAssignedDataUpdate(EuroScopePlugIn:
 							this->processed[callsign].request = true;
 						}
 					}
-					/*vsid::fpln::removeScratchPad(cad, scratchpad.substr(pos, scratchpad.size()));*/
 					vsid::fpln::removeScratchPad(FlightPlan, scratchpad.substr(pos, scratchpad.size()));
 				}
 				catch (std::out_of_range)
@@ -2672,36 +2668,25 @@ void vsid::VSIDPlugin::OnFlightPlanControllerAssignedDataUpdate(EuroScopePlugIn:
 				std::string toFind = ".VSID_STATE_";
 				size_t pos = scratchpad.find(toFind);
 
-				try
-				{
-					std::vector<std::string> states = vsid::utils::split(scratchpad.substr(pos + toFind.size(), scratchpad.size()), '/');
-					bool clrf = (states.at(0) == "TRUE") ? true : false;
-					std::string state = (states.size() > 1) ? states.at(1) : "";
+				bool clrf = scratchpad.substr(pos + toFind.size(), scratchpad.size()) == "TRUE" ? true : false;
 
-					messageHandler->writeMessage("DEBUG", "Sync state. Ground state: " + state, vsid::MessageHandler::DebugArea::Dev);
+				// DEV
+				std::string sclrf = clrf ? "TRUE" : "FALSE";
+				vsid::messageHandler->writeMessage("DEBUG", "Scratchpad clrf: " + sclrf, vsid::MessageHandler::DebugArea::Dev);
+				// END DEV
 
-					messageHandler->writeMessage("DEBUG", "[" + callsign + "] removing scratchpad: " + scratchpad, vsid::MessageHandler::DebugArea::Dev);
-					vsid::fpln::removeScratchPad(FlightPlan, scratchpad.substr(pos, scratchpad.size()));
+				if (clrf && !FlightPlan.GetClearenceFlag()) this->callExtFunc(callsign.c_str(), NULL, EuroScopePlugIn::TAG_ITEM_TYPE_CALLSIGN,
+																			callsign.c_str(), NULL, EuroScopePlugIn::TAG_ITEM_FUNCTION_SET_CLEARED_FLAG);
 
-					if (clrf && !FlightPlan.GetClearenceFlag()) this->callExtFunc(callsign.c_str(), NULL, EuroScopePlugIn::TAG_ITEM_TYPE_CALLSIGN,
-																				callsign.c_str(), NULL, EuroScopePlugIn::TAG_ITEM_FUNCTION_SET_CLEARED_FLAG);
-
-					if (state != "")
-					{
-						messageHandler->writeMessage("DEBUG", "[" + callsign + "] calling set with state: " + state, vsid::MessageHandler::DebugArea::Dev);
-						vsid::fpln::setScratchPad(FlightPlan, state);
-						//vsid::fpln::removeScratchPad(FlightPlan, "NOSTATE");
-					}
-				}
-				catch (std::out_of_range)
-				{
-					messageHandler->writeMessage("ERROR", "[" + callsign + "] failed to sync states to new ATC.");
-				}
+				vsid::fpln::removeScratchPad(FlightPlan, scratchpad.substr(pos, scratchpad.size()));
 			}
 		}
 
 	}
-	else if (this->processed.contains(callsign))
+	
+	// get ES gnd states
+
+	if (this->processed.contains(callsign))
 	{
 		if (DataType == EuroScopePlugIn::CTR_DATA_TYPE_GROUND_STATE) this->processed[callsign].gndState = FlightPlan.GetGroundState();
 	}
