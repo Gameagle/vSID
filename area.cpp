6de135702ac10b455ab9440c48cf7659a4294891
Phil7789 <87429599+Phil7789@users.noreply.github.com>
2026-04-16 19:02:16 +0200

    Remove doubled code

--- area.cpp
+++ area.cpp
@@ -59,38 +59,6 @@ vsid::Area::Point vsid::Area::toPoint(std::pair<std::string, std::string> &pos)
 	return {lat, lon};
 }
 
-//double vsid::Area::toDeg(std::string& coord)
-//{
-//	std::vector<std::string> dms = vsid::utils::split(coord, '.');
-//	int multi = 0; // default state in exception case
-//
-//	try
-//	{
-//		if (dms.size() < 4)
-//			throw std::out_of_range("Coordinate string \"" + coord + "\" does not contain enough parts for DMS conversion!");
-//
-//		multi = (dms.at(0).find('S') != std::string::npos || dms.at(0).find('W') != std::string::npos) ? -1 : 1;
-//
-//		double deg = std::stod(dms[0].substr(1, dms[0].length()));
-//		double min = std::stod(dms[1]) / 60;
-//		double sec = (std::stod(dms[2]) + std::stod("0." + dms[3])) / 3600;
-//
-//		return (deg + min + sec) * multi;
-//	}
-//	catch (std::out_of_range &e)
-//	{
-//		messageHandler->writeMessage("ERROR", "Out of bounds while calculating coordinate: " + coord + ". " + e.what());
-//	}
-//	catch (const std::invalid_argument& e)
-//	{
-//		messageHandler->writeMessage("ERROR", "Invalid number format in coord " + coord + ". " + e.what());
-//	}
-//
-//	messageHandler->writeMessage("WARNING", "Fallback state for \"" + coord + "\"! Failed to calculate. DMS will be set to 0.0");
-//
-//	return 0.0;
-//}
-
 bool vsid::Area::inside(const EuroScopePlugIn::CPosition& fplnPos)
 {
 	std::pair<Point, Point> l5 = { {fplnPos.m_Latitude, fplnPos.m_Longitude}, {fplnPos.m_Latitude + 0.05, fplnPos.m_Longitude + 0.05} };
