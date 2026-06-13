fadba7809b6832777d1c38bcf14e384bfdd35fb7
Phil7789 <87429599+Phil7789@users.noreply.github.com>
2026-06-13 17:03:51 +0200

    Add case insensitive lookup and refactor trim to string_view

--- utils.cpp
+++ utils.cpp
@@ -6,98 +6,30 @@
 #include <algorithm>
 #include <format>
 
-std::string vsid::utils::ltrim(const std::string& string)
-{
-	std::string string_to_trim = string;
-	string_to_trim.erase(string.find_last_not_of(' ') + 1);
-	return string_to_trim;
-}
-
-std::string vsid::utils::rtrim(const std::string& string)
-{
-	std::string string_to_trim = string;
-	string_to_trim.erase(0, string.find_first_not_of(' '));
-	return string_to_trim;
-}
-
-std::string vsid::utils::trim(const std::string& string)
-{
-	return vsid::utils::ltrim(vsid::utils::rtrim(string));
-}
-
-std::vector<std::string> vsid::utils::split(const std::string &string, const char &del, const bool keepWhitespace)
-{
-	std::istringstream ss(string);
-	std::vector<std::string> elems;
-	std::string elem;
-
-	while (std::getline(ss, elem, del))
-	{
-		if (elem == "" && !keepWhitespace) continue; // remove excessive whitespaces to prevent a crash caused by wrong routes
-		elems.push_back(vsid::utils::trim(elem));
-	}
-	return elems;
-}
-
-std::string vsid::utils::join(const std::vector<std::string>& toJoin, const char del)
-{
-	if (toJoin.empty()) return "";
-	std::ostringstream ss;
-	for (const auto& elem : toJoin) // possible improvement back to a copy function
-	{
-		ss << elem << del;
-	}
-	std::string joinedStr = ss.str();
-	return joinedStr.erase(joinedStr.length() - 1, 1);
-}
-
-std::string vsid::utils::join(const std::set<std::string>& toJoin, char del)
-{
-	if (toJoin.empty()) return "";
-	std::ostringstream ss;
-	for (const auto& elem : toJoin) // possible improvement back to a copy function
-	{
-		ss << elem << del;
-	}
-	std::string joinedStr = ss.str();
-	return joinedStr.erase(joinedStr.length() - 1, 1);
-}
-
-std::vector<std::string> vsid::utils::splitRoute(std::string& string)
+std::vector<std::string> vsid::utils::split(std::string_view sv, const char del, const bool keepEmpty)
 {
 	std::vector<std::string> elems;
-	std::string elem;
-	size_t pos = 0;
 
-	string = vsid::utils::trim(string);
+	size_t start = 0;
+	size_t end = sv.find(del);
 
-	while ((pos = string.find(' ')) != std::string::npos)
+	while (end != std::string_view::npos)
 	{
-		elem = string.substr(0, pos);
-		if (elem.find('/') != std::string::npos)
-		{
-			elem = vsid::utils::split(elem, '/').at(0);
-		}
-		elems.push_back(elem);
-		string.erase(0, pos + 1);
-		vsid::utils::ltrim(string);
+		auto next = vsid::utils::trim(sv.substr(start, end - start));
+
+		if (keepEmpty || !next.empty())
+			elems.emplace_back(std::move(next));
+
+		start = end + 1;
+		end = sv.find(del, start);
 	}
 
-	elems.push_back(string);
+	auto last = vsid::utils::trim(sv.substr(start));
+
+	if (keepEmpty || !last.empty())
+		elems.emplace_back(std::move(last));
 
 	return elems;
-}
-
-bool vsid::utils::isIcaoInVector(const std::vector<vsid::Airport>& airportVector, const std::string& toSearch)
-{
-	for (const vsid::Airport &elem : airportVector)
-	{
-		if (elem.icao == toSearch)
-		{
-			return true;
-		}
-	}
-	return false;
 }
 
 bool vsid::utils::containsDigit(int number, int digit)
