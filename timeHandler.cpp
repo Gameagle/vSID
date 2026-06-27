5e8382cfac0925ca931405cc732845de9ef0bca8
Phil7789 <87429599+Phil7789@users.noreply.github.com>
2026-06-27 23:42:04 +0200

    Reduce active time calculation code

--- timeHandler.cpp
+++ timeHandler.cpp
@@ -1,35 +1,41 @@
 #include "pch.h"
 #include "timeHandler.h"
-#include "messageHandler.h"
-
-#include "logger.h"
-#include <format>
 
 bool vsid::time::isActive(const std::string& timezone, const int start, const int end)
 {
 	try
 	{
-		std::chrono::zoned_time zt{ timezone, std::chrono::system_clock::now() };
-		std::chrono::zoned_time ztStart = zt;
-		std::chrono::zoned_time ztEnd = zt;
+		const std::chrono::time_zone* tz = vsid::time::getCachedTimeZone(timezone);
 
-		auto day = std::chrono::floor<std::chrono::days>(zt.get_local_time());
-		ztStart = day + std::chrono::hours{ start };
-		ztEnd = day + std::chrono::hours{ end };
+		auto localNow = tz->to_local(std::chrono::system_clock::now());
+		auto day = std::chrono::floor<std::chrono::days>(localNow);		
 
-		if ((ztEnd.get_local_time() < ztStart.get_local_time() &&
-			(zt.get_local_time() > ztStart.get_local_time() || zt.get_local_time() < ztEnd.get_local_time())) ||
-			(ztEnd.get_local_time() > ztStart.get_local_time() &&
-			zt.get_local_time() > ztStart.get_local_time() &&
-			zt.get_local_time() < ztEnd.get_local_time())
-			)
+		auto ztStart = day + std::chrono::hours{ start };
+		auto ztEnd = day + std::chrono::hours{ end };
+
+		if (ztStart <= ztEnd)
 		{
-			return true;
+			return localNow >= ztStart && localNow < ztEnd;
 		}
+
+		return localNow >= ztStart || localNow < ztEnd;
 	}
-	catch (std::runtime_error& e)
+	catch (const std::runtime_error& e)
 	{
-		vsid::Logger::log(vsid::LogLevel::Error, std::format("Timezone failed - {}: {}", e.what(), timezone));
+		vsid::Logger::log(vsid::LogLevel::Error, std::format("Time calculation failed [{}] - {}", timezone, e.what()));
 	}
 	return false;
+}
+
+std::string vsid::time::getFormattedTime(std::chrono::system_clock::time_point tp, std::string_view fmtStr)
+{
+	std::time_t time = std::chrono::system_clock::to_time_t(tp);
+	std::tm tm{};
+	gmtime_s(&tm, &time);
+
+	std::array<char, 64> timeBuffer;
+
+	if (std::strftime(timeBuffer.data(), timeBuffer.size(), fmtStr.data(), &tm) > 0) return std::string(timeBuffer.data());
+
+	return "TIME_ERROR";
 }
